43f6aaf4417e18c30271ce416097e73a1124abef
oezios <oezios.normando@gmail.com>
2024-01-25 00:51:44 -0400

    corrigindo erro de mais de um usuario com o mesmo nome

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
 FLASK_APP=blogbacalhau.py
+FLASK_DEBUG=1
--- app/forms.py
+++ app/forms.py
@@ -42,3 +42,14 @@ class EditProfileForm(FlaskForm):
     username = StringField('Usuário', validators=[DataRequired()])
     about_me = TextAreaField('Sobre mim', validators=[Length(min=0, max=140)])
     submit = SubmitField('Salvar')
+    
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(
+                User.username == self.username.data))
+            if user is not None:
+                raise ValidationError('Please use a different username.')
